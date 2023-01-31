815586546cdc5e2f12ed7e31f2ca4253ef66e853
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-01-31 19:41:51 +0000

    javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,38 @@
+var orderHistory = [
+  {
+    productName: 'JavaScript for impatient programmers',
+    author: 'Dr.Axel Rauschmayer',
+    returnWindowDate: '2020-09-07',
+    productPrice: 31.55,
+    orderPlaced: '2020-08-04',
+    totalWithTax: 34.00,
+    shipTo: 'JA Masher',
+    'Order #': 114 - 3941689 - 8772232,
+    'Delivery Date': '2020-08-08'
+  },
+  {
+    productName: 'The Timeless Way of Building',
+    author: 'Christopher Alexander',
+    returnWindowDate: '2020-08-29',
+    productPrice: 41.33,
+    orderPlaced: '2020-07-19',
+    totalWithTax: 44.53,
+    shipTo: 'JA Masher',
+    orderNumber: 113 - 99984268 - 1280257,
+    deliveryDate: '2020-07-20'
+  },
+  {
+    orderNumber: 114 - 2875557 - 9059409,
+    totalWithTax: 17.22,
+    orderPlaced: '2020-07-04',
+    shipTo: 'JA Masher',
+    deliveryDate: '2020-07-07',
+    returnWindowDate: '2020-08-05',
+    product: [
+      {
+        productName: 'Gamecube Controller Adapter',
+        productPrice: 15.98
+      }
+    ]
+  }
+];
