e6cb4aa86abab7fe20c90edb228684c0fd0c76ea
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-02-04 08:40:28 +0000

    commit 1

---
+++ string-manipulation-2/src/capitalize-words.js
@@ -0,0 +1,14 @@
+/* exported capitalizeWords */
+/* function capitalizeWord(word) {
+  word = word.toLowerCase();
+  console.log(word);
+  if (word !== 'javascript') {
+    var firstChar = word[0];
+    console.log('firstChar', firstChar);
+    console.log('word[0]', word[0]);
+    console.log('toUpperCase()', firstChar.toUpperCase());
+    word.replace(word[0], firstChar.toUpperCase());
+  }
+
+  console.log(word);
+} */
