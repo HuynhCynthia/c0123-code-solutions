7bd891505b135f22f89acc1fb4ce632c86364436
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-02-07 18:07:29 +0000

    progress

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,12 @@
 /* exported capitalizeWord */
+function capitalizeWord(word) {
+  var newWord = word.toLowerCase();
+  if (word !== 'javascript') {
+    var firstChar = word[0];
+    var upperCaseWord = newWord.replace(word[0], firstChar.toUpperCase());
+  } else {
+    upperCaseWord = 'JavaScript';
+  }
+
+  return upperCaseWord;
+}
--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,12 +1,1 @@
 /* exported capitalizeWords */
-function capitalizeWords(word) {
-  word = word.toLowerCase();
-  if (word !== 'javascript') {
-    var firstChar = word[0];
-    word = word.replace(word[0], firstChar.toUpperCase());
-  } else {
-    word = 'JavaScript';
-  }
-
-  return word;
-}
