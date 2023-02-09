13817e113a72f6dc98478d9c7c7a5f2621ba4ef1
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-02-09 22:26:03 +0000

    progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,13 +1,16 @@
 /* exported capitalizeWords */
-/* function capitalizeWords(string) {
+function capitalizeWords(string) {
   var lowerCaseString = string.toLowerCase();
   var stringArray = lowerCaseString.split(' ');
-  console.log('stringArray:', stringArray);
+  var newString;
+  // console.log('stringArray:', stringArray);
   for (let i = 0; i < stringArray.length; i++) {
     var word = stringArray[i];
     var upperFirstChar = word[0].toUpperCase();
-    console.log('upperFirstChar:', upperFirstChar);
-    word.replace(word[0], upperFirstChar);
-    console.log('new uppercased word:', word);
+    // console.log('upperFirstChar:', upperFirstChar);
+    var upperCasedWord = word.replace(word[0], upperFirstChar);
+    // console.log('new uppercased word:', upperCasedWord);
+    // console.log('hello:', newString);
   }
-} */
+  return upperCasedWord + newString;
+}
