5b6492075d086cc81d627cee386d296011077e76
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-02-09 20:07:23 +0000

    progress

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,13 +1,14 @@
 /* exported ransomCase */
 function ransomCase(string) {
-  // var lowerCaseString = string.toLowerCase();
-  // var newString;
+// var lowerCaseString = string.toLowerCase();
+// var newString;
 
   for (let i = 0; i < string.length; i++) {
     if (i % 2 !== 0) {
-      // newString += lowerCaseString[i];
+    //  newString += lowerCaseString[i];
+    } else {
+    // newString += lowerCaseString[i].toUpperCase;
     }
   }
 
-  // console.log(newString);
 }
