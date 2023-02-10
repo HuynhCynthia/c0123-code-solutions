3da093e23647a00c3157eec286d9aa2b3e0644dc
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-02-10 22:56:04 +0000

    string-manipulation-2 - Cynthia Huynh

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,8 +1,14 @@
 /* exported swapChars */
-/* function swapChars(firstIndex, secondIndex, string) {
-  var firstChar = string[firstIndex];
-  var secondChar = string[secondIndex];
-  var firstStringSwitch = string.replace(string[0], secondChar);
-  var secondStringSwitch = firstStringSwitch.replace(string[1], firstChar);
-  console.log(secondStringSwitch);
-} */
+function swapChars(firstIndex, secondIndex, string) {
+  var newString = [];
+  for (let i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      newString[secondIndex] = string[firstIndex];
+    } else if (i === secondIndex) {
+      newString[firstIndex] = string[secondIndex];
+    } else {
+      newString[i] = string[i];
+    }
+  }
+  return newString.join('');
+}
