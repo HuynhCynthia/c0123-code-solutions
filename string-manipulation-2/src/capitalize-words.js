ac1add4c947c929b2e53b3904fb0471d4ffc2bf7
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-02-10 05:09:10 +0000

    progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -2,15 +2,14 @@
 function capitalizeWords(string) {
   var lowerCaseString = string.toLowerCase();
   var stringArray = lowerCaseString.split(' ');
-  var newString;
+  var newString = [];
   // console.log('stringArray:', stringArray);
   for (let i = 0; i < stringArray.length; i++) {
     var word = stringArray[i];
     var upperFirstChar = word[0].toUpperCase();
-    // console.log('upperFirstChar:', upperFirstChar);
     var upperCasedWord = word.replace(word[0], upperFirstChar);
-    // console.log('new uppercased word:', upperCasedWord);
-    // console.log('hello:', newString);
+    newString.push(upperCasedWord);
   }
-  return upperCasedWord + newString;
+
+  return newString.join(' ');
 }
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,13 @@
 /* exported numVowels */
+function numVowels(string) {
+  var newString = string.toLowerCase();
+  var vowelCount = 0;
+
+  for (let i = 0; i < newString.length; i++) {
+    if (newString[i] === 'a' || newString[i] === 'e' || newString[i] === 'i' || newString[i] === 'o' || newString[i] === 'u') {
+      vowelCount++;
+    }
+  }
+
+  return vowelCount;
+}
