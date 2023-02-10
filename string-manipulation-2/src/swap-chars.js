ab97c05c8d25d363a026ea50f3a0b5e2d12b85ad
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-02-10 18:32:51 +0000

    swap-chars

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,8 @@
 /* exported swapChars */
+/* function swapChars(firstIndex, secondIndex, string) {
+  var firstChar = string[firstIndex];
+  var secondChar = string[secondIndex];
+  var firstStringSwitch = string.replace(string[0], secondChar);
+  var secondStringSwitch = firstStringSwitch.replace(string[1], firstChar);
+  console.log(secondStringSwitch);
+} */
