65fc520f6b41a7cb4bfe26feb5640d8387cc41b7
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-02-21 18:33:18 +0000

    created student constructor as well as prototype for fullname and intro

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,19 @@
+var exObject = new Object();
+var exArray = new Array();
+var exFunction = new Function();
+var exString = new String();
+var exNumber = new Number();
+var exBoolean = new Boolean();
+
+console.log('value of anObject:', exObject);
+console.log('typeof anObject:', typeof exObject);
+console.log('value of exArray:', exArray);
+console.log('typeof exArray:', typeof exArray);
+console.log('value of exFunction:', exFunction);
+console.log('typeof exFunction:', typeof exFunction);
+console.log('value of exString:', exString);
+console.log('typeof exString:', typeof exString);
+console.log('value of exNumber:', exNumber);
+console.log('typeof exNumber:', typeof exNumber);
+console.log('value of exBoolean:', exBoolean);
+console.log('typeof exBoolean:', typeof exBoolean);
